197bf4042dfcf02ad2d0aaa8cdeb4bb1240c0f23
Ash <168602933+AscheKumo@users.noreply.github.com>
2025-02-20 19:23:54 -0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@ var firebaseConfig = {
   apiKey: "AIzaSyBtcV1JZX8Xmc374WcCD1CJ68pGLEQpnUc",
   authDomain: "sacredgg-a171a.firebaseapp.com",
   projectId: "sacredgg-a171a",
-  storageBucket: "sacredgg-a171a.firebasestorage.app", // <-- Check this value!
+  storageBucket: "sacredgg-a171a.appspot.com", // note "appspot.com" here
   messagingSenderId: "416265042350",
   appId: "1:416265042350:web:75cf8705a1fb1f5174b03f",
   measurementId: "G-EC25JFK8S7"
