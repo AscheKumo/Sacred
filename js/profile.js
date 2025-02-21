26f7d35cf49e319de23338ecc7c0e3211384796f
Ash <168602933+AscheKumo@users.noreply.github.com>
2025-02-20 18:44:06 -0700

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,10 @@
 // js/profile.js
 firebase.auth().onAuthStateChanged(function(user) {
   if (!user) {
+    console.log("No user detected in profile.js; redirecting to login.");
     window.location.href = "index.html";
   } else {
+    console.log("User detected in profile.js:", user.email);
     var db = firebase.firestore();
     var charRef = db.collection("characters").doc(user.uid);
     charRef.get().then(function(doc) {
@@ -16,12 +18,14 @@ firebase.auth().onAuthStateChanged(function(user) {
         if(data.bio) {
           document.getElementById("bio-input").value = data.bio;
         }
+        console.log("Profile data loaded for:", data.username);
       }
     });
     
     document.getElementById("save-bio-btn").addEventListener("click", function() {
       var bio = document.getElementById("bio-input").value;
       charRef.update({ bio: bio }).then(function() {
+        console.log("Bio updated for user.");
         alert("Bio saved!");
       });
     });
