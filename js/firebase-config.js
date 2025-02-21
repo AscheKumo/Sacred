f2cc417248beb5dfb3aef753ebf9609b4544f4f9
Ash <168602933+AscheKumo@users.noreply.github.com>
2025-02-20 18:49:09 -0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,12 @@
 // js/firebase-config.js
 var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBtcV1JZX8Xmc374WcCD1CJ68pGLEQpnUc",
+  authDomain: "sacredgg-a171a.firebaseapp.com",
+  projectId: "sacredgg-a171a",
+  storageBucket: "sacredgg-a171a.firebasestorage.app",
+  messagingSenderId: "416265042350",
+  appId: "1:416265042350:web:75cf8705a1fb1f5174b03f",
+  measurementId: "G-EC25JFK8S7"
 };
 
 console.log("Initializing Firebase with config:", firebaseConfig);
